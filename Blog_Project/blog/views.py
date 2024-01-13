d05d3967411f948ffb1b87d6e3444a99ab0030e6
Bharatk003 <bharatkshirsagar003@gmail.com>
2024-01-13 11:49:50 +0530

    add form and sign up page

--- Blog_Project/blog/views.py
+++ Blog_Project/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 from .models import Post
 class BlogListView(ListView):
@@ -16,3 +18,24 @@ class BlogDetailView(DetailView):
     #in DetailView there is the context_object_name = object/post, it is default for the DetailView
     #context_object only gives us an access to the one object at a time as per the primary key (pk )
 
+class BlogCreateView(CreateView):
+
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+
+
+class BlogDeleteView(DeleteView):
+
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+
+ 
