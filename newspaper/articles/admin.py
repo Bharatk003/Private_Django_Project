6db0d49151fa00c1fdd3289d08685918a6e6b211
Bharatk003 <bharatkshirsagar003@gmail.com>
2024-01-22 17:24:13 +0530

    completed newspaper web

--- newspaper/articles/admin.py
+++ newspaper/articles/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Article)
+class CommentInline(admin.StackedInline): # new
+    model = models.Comment
+ 
+
+class ArticleAdmin(admin.ModelAdmin): # new
+    inlines = [
+    CommentInline,
+    ]
+
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
+
+class CommentInline(admin.StackedInline):
+    model = models.Comment
+    extra = 0 # new
--- newspaper/articles/models.py
+++ newspaper/articles/models.py
@@ -18,4 +18,26 @@ class Article(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('article_datals', args=[str(self.id)])
+        return reverse('article_detail', args=[str(self.id)])
+
+
+class Comment(models.Model): # new
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name = 'comments',)
+
+    comment = models.CharField(max_length=200)
+
+    author = models.ForeignKey(
+    get_user_model(),
+    on_delete=models.CASCADE,
+
+    )
+
+
+    def __str__(self):
+        return self.comment
+    
+
+    def get_absolute_url(self):
+        return reverse('article_list')
+ 
